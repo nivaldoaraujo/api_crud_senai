a5153c2acab2ea54e13d4ed2c4cd99e5dcce81de
nivaldoaraujo <batista_nba@hotmail.com>
2023-11-08 10:50:50 -0300

    projeto deploy

---
+++ src/Componentes/Rodape.jsx
@@ -0,0 +1,18 @@
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function Rodape(){
+    return (
+<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          
+        </Container>
+      </Navbar>
+    )
+}
+export default Rodape
